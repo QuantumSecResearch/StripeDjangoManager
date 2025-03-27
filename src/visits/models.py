82d41bb9d6afe795af8694077a3d57732847e117
QuantumSecResearch <elghazaly.aiman@uit.ac.ma>
2025-03-27 01:06:32 +0000

    changesetting

--- src/visits/models.py
+++ src/visits/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class PageVisit(models.Model):
-    #db->table
-    #id->primary 
-    path = models.TextField(blank=True,null=True)#col
-    timestamp = models.DateTimeField(auto_now_add=True)#col
+    path = models.CharField(max_length=255, null=False, blank=False) 
+    timestamp = models.DateTimeField(auto_now_add=True)  # Champ automatiquement ajouté lors de la création d'un enregistrement
+ 
