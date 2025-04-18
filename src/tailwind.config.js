a14c73a72ed239ba3f19fa93120184ebc3e36411
QuantumSecResearch <elghazaly.aiman@uit.ac.ma>
2025-04-18 17:05:53 +0100

    all module

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+export const content = [
+    './templates/**/*.html', // Inclure vos fichiers HTML dans le dossier templates
+    './src/**/*.html', // Inclure vos fichiers HTML dans le dossier src
+    './src/**/*.js', // Inclure vos fichiers JS
+    './src/**/*.py', // Inclure vos fichiers Python (si nécessaire pour Django)
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
